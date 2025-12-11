9f73737c27ab8e09dbdc43ba0109785d2cf616d1
AbdelrahmanSoliman <a.soliman@uni.minerva.edu>
2025-12-11 15:56:28 -0300

    fixed-small-tweaks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import SignupPage from './features/auth/pages/signup'
 import LoginPage from './features/auth/pages/login'
 import ProfilePage from './features/profile/ProfilePage';
+import { getAccessToken } from './features/auth/services/authservice'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<Navigate to={getAccessToken() ? "/profile" : "/login"} replace />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/profile" element={<ProfilePage />} />
